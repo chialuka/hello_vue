340243a5e48e476a08823de134e677f81a660314
Chukwualuka Chiama <lukaschiama@gmail.com>
2019-11-12 21:16:15 +0100

    setup project with vue

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,27 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h2>Quick Notes</h2>
+    <RiteIt />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import RiteIt from './components/note.vue';
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    RiteIt
   }
 }
 </script>
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Montserrat", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  margin-top: 0;
 }
 </style>
---
+++ src/components/note.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="note">
+    <section>
+      <section class="new-note" v-on:click="createNote"></section>
+      <textarea type="text" class="input-area" />
+    </section>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "note",
+  method: {
+    createNote: () => {}
+  }
+};
+</script>
+
+<style scoped>
+.new-note {
+  background-color: rgb(240, 183, 28);
+  color: #2b2b2b;
+  height: 200px;
+  width: 200px;
+}
+.input-area {
+  overflow: hidden;
+  background-color: rgb(240, 183, 28);
+  border: none;
+  color: #2b2b2b;
+  height: 200px;
+  width: 200px;
+}
+</style>
